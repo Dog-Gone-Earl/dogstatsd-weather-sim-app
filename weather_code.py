e4fe08983958a12f1668ab52cb437c08940b0d09
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2023-01-09 16:48:56 -0700

    Updated of code
    Added count, distribution, and histogram values

--- weather_code.py
+++ weather_code.py
@@ -1,20 +1,42 @@
+from datadog import initialize, statsd
+import time
+import random
+
+options = {
+    'statsd_host':'127.0.0.1',
+    'statsd_port':8125
+}
+
+initialize(**options)
+
 while True:
-  
+
   temperature = random.randint(70,75)
   humidity = random.randint(30,35)
   pressure = random.randint(800,1000)
  
   #Count
-  
-  #Rate
-
+  statsd.increment('temperature.count.increment', temperature, tags=["environment:dev"])
+  statsd.decrement('temperature.count.decrement', temperature, tags=["environment:dev"])  
+  statsd.increment('humidity.count.increment', humidity, tags=["environment:dev"])
+  statsd.decrement('humidity.count.decrement', humidity, tags=["environment:dev"])  
+  statsd.increment('pressuse.count.increment', pressure, tags=["environment:dev"])
+  statsd.decrement('pressure.count.decrement', pressure, tags=["environment:dev"]) 
+ 
   #Gauge
-  statsd.gauge('current.temperature.gauge', temperature,tags=["environment:dev"])
-  statsd.gauge('current.humidity.gauge',humidity, tags=["environment:dev"])
-  statsd.gauge('current.pressure.gauge',pressure, tags=["environment:dev"])
+  statsd.gauge('temperature.gauge', temperature,tags=["environment:dev"])
+  statsd.gauge('humidity.gauge',humidity, tags=["environment:dev"])
+  statsd.gauge('pressure.gauge',pressure, tags=["environment:dev"])
  
-  #Histogram (.avg, .median, .count)
-
+  #Histogram (.avg, .median, .count, .max, .95percentile)
+  statsd.histogram('temperature.histogram', temperature, tags=["environment"])
+  statsd.histogram('humidity.histogram', humidity, tags=["environment"])
+  statsd.histogram('pressure.histogram', pressure, tags=["environment"])
+  
   #Distribution (avg, count, max, min)
- 
+  statsd.distribution('temperature.distribution', temperature, tags=["environment"])
+  statsd.distribution('humidity.distribution', humidity, tags=["environment"])
+  statsd.distribution('pressure.distribution', pressure, tags=["environment"])
+
+
   time.sleep(10)
