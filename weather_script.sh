5681fec4fe1cdc2692da701e5d674adfa6c17358
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2023-01-09 16:59:04 -0700

    Update weather_script.sh

--- weather_script.sh
+++ weather_script.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Updates! Updates! Updates!"
+sleep 5
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 echo "Installing pip and datadog"
+sleep 5
 sudo apt install python3-pip
 pip3 install datadog
 
 echo "Retrieving Python file"
+sleep 5
 curl https://raw.githubusercontent.com/Dog-Gone-Earl/dogstatsd-weather-sim-app/main/weather_code.py
