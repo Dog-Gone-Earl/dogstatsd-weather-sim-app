5b420ae7ac56d34679cd8bbbf152808dbcc213aa
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2023-01-09 17:29:10 -0700

    Update weather_script.sh

--- weather_script.sh
+++ weather_script.sh
@@ -6,7 +6,7 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 
 echo "Installing pip and datadog"
 sleep 5
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip3 install datadog
 
 echo "Retrieving Python file"
